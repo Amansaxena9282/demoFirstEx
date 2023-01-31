ac4acc9702949f57152928da8bade65401849a8e
aman <aman123@gmail.com>
2023-01-31 17:33:34 +0530

    code was commited

---
+++ src/main/java/com/example/demoFirstEx/DemoFirstExApplication.java
@@ -0,0 +1,17 @@
+package com.example.demoFirstEx;
+
+import com.example.demoFirstEx.FirstController.FirstController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFirstExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFirstExApplication.class, args);
+	}
+//FirstController firstController=new FirstController();
+//		firstController.setName(" aman");
+//	System.out.println("aligners="+FirstController.getName());
+
+}
---
+++ src/test/java/com/example/demoFirstEx/DemoFirstExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoFirstEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFirstExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
